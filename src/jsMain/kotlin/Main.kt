7ff281689724c887cdc39575c72cfd6de3b88c8f
Julien Lengrand-Lambert <julien@lengrand.fr>
2022-03-31 10:47:53 +0200

    First complete version

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -6,22 +6,32 @@ import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 import kotlin.js.Promise
 
-data class FirebaseUser(
-    val email: String,
-    val uid: String,
-    val accessToken: String,
-)
+external interface FirebaseUser{
+    val email: String
+    val uid: String
+    val accessToken: String
+}
+
+external interface FirestoreMessage{
+    val id: String
+    val content: String
+}
 
 @JsModule("@jlengrand/firebase-ports")
 @JsNonModule
 external object FirebasePorts{
     fun logIn() : Promise<FirebaseUser>
     fun logOut()
+
+    fun saveMessage(uid: String, message: String)
+    fun getMessages(uid: String) : Promise<Array<FirestoreMessage>>
 }
 
 fun main() {
     var user : FirebaseUser? by mutableStateOf(null)
     var error : String? by mutableStateOf(null)
+    var message : String by mutableStateOf("")
+    var messages : Array<FirestoreMessage>? by mutableStateOf(null)
 
     renderComposable(rootElementId = "root") {
 
@@ -69,10 +79,77 @@ fun main() {
                     Text(JSON.stringify(user))
                 }
             }
+
+            Div({ style { padding(25.px) } }){
+                P(){
+                    Text("---------------")
+                }
+
+                TextArea(value = message,
+                    attrs = {
+                    onInput {
+                        console.log(it.value)
+                        message = it.value
+                    }
+                })
+
+                Button(attrs = {
+                    onClick {
+                        error = null
+                        console.log(JSON.stringify(user))
+                        console.log(user)
+                        console.log(user!!.uid)
+                        FirebasePorts.saveMessage(user!!.uid, message)
+                        message = ""
+                    }
+                }) {
+                    Text("Save!")
+                }
+            }
+
+            Div({ style { padding(25.px) } }) {
+                P(){
+                    Text("---------------")
+                }
+
+                Button(attrs = {
+                    onClick {
+                        error = null
+                        FirebasePorts.getMessages(user!!.uid)
+                            .then { messages = it }
+                            .catch { error = it.message }
+                    }
+                }) {
+                    Text("Retrieve messages!")
+                }
+
+                if (messages == null){
+                    P(){
+                        Text("No messages found")
+                    }
+                }
+                else{
+                    Ul(){
+                        for (m in messages!!){
+                            Li(){
+                                Text("${m.content} with id : ${m.id}")
+                            }
+                        }
+                    }
+                }
+
+                P(){
+                    Text("---------------")
+                }
+            }
         }
 
         if (error != null) {
             Div({ style { padding(25.px) } }){
+                P(){
+                    Text("---------------")
+                }
+
                 P() {
                     Text("Error: ")
                     Text(error!!)
--- src/jsMain/resources/firebase-ports/index.js
+++ src/jsMain/resources/firebase-ports/index.js
@@ -1,10 +1,12 @@
 import {FIREBASE_CONFIG} from "./constants";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { collection, addDoc, getFirestore, getDocs}  from "firebase/firestore";
 
 const firebaseApp = initializeApp(FIREBASE_CONFIG);
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
+const firestore = getFirestore(firebaseApp);
 
 export async function logIn(){
 
@@ -22,4 +24,27 @@ export async function logIn(){
 export async function logOut(){
     console.log("Logging out!");
     await signOut(auth);
+}
+
+export async function saveMessage(uid, message){
+    console.log("Saving message : " + message + " for user " + uid + "!");
+
+    await addDoc(collection(firestore, `users/${uid}/messages`), {
+        content : message
+    });
+}
+
+export async function getMessages(uid){
+    console.log("Retrieving existing messages");
+    let messages = [];
+
+    const querySnapshot = await getDocs(collection(firestore, `users/${uid}/messages`));
+    querySnapshot.forEach((doc) => {
+        messages.push({
+            id: doc.id,
+            content: doc.data().content
+        })
+    });
+
+    return messages;
 }
